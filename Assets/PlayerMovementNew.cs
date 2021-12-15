6172f1239618c8b3f8562a24b2e341b0d1978ea8
lbondya <lbondyaaa@gmail.com>
2021-12-16 01:37:50 +0300

    Initial commit

---
+++ Assets/PlayerMovementNew.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementNew : MonoBehaviour
+{
+    public float Speed = 10;
+     Rigidbody m_Rigidbody;
+    private Vector3 _direction;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        Debug.Log(_direction);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed;
+    }
+}
+
